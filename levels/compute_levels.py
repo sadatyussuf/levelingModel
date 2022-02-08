afdb98e5b118a302ed07dc65254affe2b9ed6955
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-02-08 15:15:55 +0000

    finished reduced_level,check_calculation,adjusted and adj_reduced_level functions

--- levels/compute_levels.py
+++ levels/compute_levels.py
@@ -36,7 +36,7 @@ def extract_tbm(stations):
     for stn in stations:
         if stn.tbm:
             tbms.append(stn.tbm)
-    print(tbms)
+    # print(tbms)
     return tbms
 
 
@@ -44,5 +44,66 @@ def level_diff(stations):
     all_level_diff = []
     for stn in stations:
         all_level_diff.append(stn.level_difference())
-    print(all_level_diff)
+    # print(all_level_diff)
     return all_level_diff
+
+
+def reduced_level(level_diff, tbms):
+    c = 0
+    initial_rl = tbms[0]
+    rd_level = []
+    rd_level.append(initial_rl)
+    for stn_data in level_diff:
+        for data in stn_data:
+            rd_lv = data + rd_level[c]
+            rd_level.append(round(rd_lv, 3))
+            c += 1
+    # print(rd_level)
+    return rd_level
+
+
+def check_calculation(level_diff, rl, tbms):
+    correction = None
+    initial_rl = rl[0]
+    final_rl = rl[-1]
+    true_final_rl = tbms[1]
+
+    rise_fall_total = 0
+    # check rise and fall total
+    for stn_data in level_diff:
+        for data in stn_data:
+            rise_fall_total += data
+    # print(rise_fall_total)
+    # check difference btn initail and final TBM
+    diff_rl = round(final_rl - initial_rl, 3)
+    # print(diff_rl)
+    if rise_fall_total == diff_rl:
+        correction = round(true_final_rl - final_rl, 3)
+    # print(f'true={true_final_rl} - obv= {final_rl} = {correction}')
+    return correction
+
+
+def adjusted(level_diff, correction):
+    # c = 0
+    # initial_rl = tbms[0]
+    num_stns = len(level_diff)
+    adj_rd_level = []
+    adj_rd_level.append(0)
+    adj_per_stn = correction/num_stns
+
+    adj = adj_per_stn
+    for stn_data in level_diff:
+        for _ in stn_data:
+            adj_rd_level.append(adj)
+        # increase by two
+        adj += adj_per_stn
+    # print(adj_rd_level)
+    return adj_rd_level
+
+
+def adj_reduced_level(reduced_level, adjusted):
+    adj_reduced_level = []
+    for i, _ in enumerate(adjusted):
+        adj_rl = round(reduced_level[i] + adjusted[i], 3)
+        adj_reduced_level.append(adj_rl)
+    print(adj_reduced_level)
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from levels.compute_levels import extract_tbm, level_diff, setStations
+from levels.compute_levels import adj_reduced_level, adjusted, check_calculation, extract_tbm, level_diff, reduced_level, setStations
 from read_csvfile import readFile
 
-# return data
+
 if __name__ == '__main__':
     data = readFile('Levelling.csv')
     stations = setStations(data)
-    diff = level_diff(stations)
-    extract_tbm(stations)
+    rise_fall = level_diff(stations)
+    tbms = extract_tbm(stations)
+    rl = reduced_level(rise_fall, tbms)
+    corrections = check_calculation(rise_fall, rl, tbms)
+    adj = adjusted(rise_fall, corrections)
+    adj_reduced_level(rl, adj)
